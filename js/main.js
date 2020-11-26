dc0aa2e0384b9efaf2cbe7ce5d63cf9527203939
SanLis Gestão <74407437+sanlisgestao@users.noreply.github.com>
2020-11-26 19:24:36 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(".planoOuro").html(planoOuro);
 
 
 
-var textoQuemSomos = "Trabalhamos em parceria presencial com empreendedores, artesãos, produtores, microempreendedores individuais e pequenas empresas com serviços especializados em gestão das atividades financeiras, comerciais, operacionais e administrativas. Nossa equipe atua com os clientes, a fim de identificar os 6 pilares ligados a melhoria dos resultados. Esses pilares são: pessoas, métodos, processos, planejamento, indicadores e tecnologia. Em uma economia competitiva e dinâmica, é cada vez mais importante que a Gestão de um negócio se adapte às novas realidades exigidas pelo mercado e pelos consumidores. Os serviços da SanLis são elaborados de acordo com a realidade de cada negócio identificando e propondo soluções adequadas ao perfil do cliente."
+var textoQuemSomos = "Trabalhamos em parceria presencial com empreendedores, artesãos, produtores, microempreendedores individuais e pequenas empresas com serviços especializados em gerenciamento das atividades financeiras, comerciais, operacionais e administrativas. Nossa equipe atua com os clientes, a fim de identificar os 6 pilares ligados a melhoria dos resultados. Esses pilares são: pessoas, métodos, processos, planejamento, indicadores e tecnologia. Em uma economia competitiva e dinâmica, é cada vez mais importante que a Gestão de um negócio se adapte às novas realidades exigidas pelo mercado e pelos consumidores. Os serviços da SanLis são elaborados de acordo com a realidade de cada negócio identificando e propondo soluções adequadas ao perfil do cliente."
 $(".textoQuemSomos").html(textoQuemSomos);
 
 
