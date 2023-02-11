3e5b5bd880895081057db1fc6bdeb680e070e620
Angelita Santos <angelitasantos@gmail.com>
2023-02-11 10:45:49 -0300

    create dataJson planos.js

---
+++ staticfiles/js/data/dataJson_planos.js
@@ -0,0 +1,23 @@
+let dataJson = [
+    {
+        id: 1,
+        img: '../../staticfiles/img/planobasico.webp',
+        title: 'Plano Básico',
+        resume: 'Quer ter o controle de suas atividades na palma da mão?',
+        link: 'templates/planos.html'
+    },
+    {
+        id: 2,
+        img: '../../staticfiles/img/planoprata.webp',
+        title: 'Plano Prata',
+        resume: 'Há quanto tempo você trabalha mais de 12 horas por dia?',
+        link: 'templates/planos.html'
+    },
+    {
+        id: 3,
+        img: '../../staticfiles/img/planoouro.jpg',
+        title: 'Plano Ouro',
+        resume: 'O que está faltando para os seus sonhos se tornarem realidade?',
+        link: 'templates/planos.html'
+    }
+];
--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -21,15 +21,6 @@ $(".emConstrucao").html(emConstrucao);
 var textoAreaRestrita = "Área Restrita"
 $(".textoAreaRestrita").html(textoAreaRestrita);
 
-var planoBasico = "Quer ter o controle de suas atividades na palma da mão?"
-$(".planoBasico").html(planoBasico);
-
-var planoPrata = "Há quanto tempo você trabalha mais de 12 horas por dia?"
-$(".planoPrata").html(planoPrata);
-
-var planoOuro = "O que está faltando para os seus sonhos se tornarem realidade? "
-$(".planoOuro").html(planoOuro);
-
 
 
 var textoQuemSomos = "Trabalhamos em parceria presencial com empreendedores, artesãos, produtores, microempreendedores individuais e pequenas empresas com serviços especializados em gerenciamento das atividades financeiras, comerciais, operacionais e administrativas. Nossa equipe atua com os clientes, a fim de identificar os 6 pilares ligados a melhoria dos resultados. Esses pilares são: pessoas, métodos, processos, planejamento, indicadores e tecnologia. Em uma economia competitiva e dinâmica, é cada vez mais importante que a Gestão de um negócio se adapte às novas realidades exigidas pelo mercado e pelos consumidores. Os serviços da SanLis são elaborados de acordo com a realidade de cada negócio identificando e propondo soluções adequadas ao perfil do cliente."
