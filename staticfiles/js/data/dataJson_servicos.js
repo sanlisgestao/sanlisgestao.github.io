71bf97632b51a51fdcc1215bc6663ae6b03caca7
Angelita Santos <angelitasantos@gmail.com>
2023-02-11 19:00:34 -0300

    create dataJson servicos.js

---
+++ staticfiles/js/data/dataJson_servicos.js
@@ -0,0 +1,45 @@
+let dataJson = [
+    {
+        id: 1,
+        title: 'Gestão Financeira',
+        resume: 'Auxiliar a reduzir despesas financeiras, melhorar o retorno sobre o patrimônio, \
+                    obter ganhos no fluxo de caixa operacional e eliminar desperdícios.',
+        link: 'index.html'
+    },
+    {
+        id: 2,
+        title: 'Gestão de Vendas',
+        resume: 'Assegurar o crescimento das vendas, a melhoria da lucratividade e a longevidade dos \
+                    resultados.  Identificar e padronizar as melhores práticas.',
+        link: 'index.html'
+    },
+    {
+        id: 3,
+        title: 'Gestão de Estoque',
+        resume: 'Equilibrar compras, armazenagem e entregas, controlando as entradas e o consumo de \
+                    materiais e movimentando o ciclo dos produtos.',
+        link: 'index.html'
+    },
+    {
+        id: 4,
+        title: 'Gestão de Compras',
+        resume: 'Otimizar os custos e o potencial aumento da lucratividade sobre as vendas atraves da \
+                    padronização do planejamento das compras com base no perfil dos seus clientes.',
+        link: 'index.html'
+    },
+    {
+        id: 5,
+        title: 'Gestão de Produção',
+        resume: 'Levantar e analisar as interrupções nos processos produtivos e as respectivas causas \
+                    dessas interrupções, viabilizando um maior ganho de produtividade.',
+        link: 'index.html'
+    },
+
+    {
+        id: 6,
+        title: 'Gestão Administrativa',
+        resume: 'Gerenciar as rotinas a fim de permitir que profissionais sejam capazes de resolver \
+                    problemas do dia a dia e de minimizar a sobrecarga de supervisores e gerentes.',
+        link: 'index.html'
+    }
+];
--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -25,25 +25,3 @@ $(".textoAreaRestrita").html(textoAreaRestrita);
 
 var textoQuemSomos = "Trabalhamos em parceria presencial com empreendedores, artesãos, produtores, microempreendedores individuais e pequenas empresas com serviços especializados em gerenciamento das atividades financeiras, comerciais, operacionais e administrativas. Nossa equipe atua com os clientes, a fim de identificar os 6 pilares ligados a melhoria dos resultados. Esses pilares são: pessoas, métodos, processos, planejamento, indicadores e tecnologia. Em uma economia competitiva e dinâmica, é cada vez mais importante que a Gestão de um negócio se adapte às novas realidades exigidas pelo mercado e pelos consumidores. Os serviços da SanLis são elaborados de acordo com a realidade de cada negócio identificando e propondo soluções adequadas ao perfil do cliente."
 $(".textoQuemSomos").html(textoQuemSomos);
-
-
-
-var textoFinanceiro = "Auxiliar a reduzir despesas financeiras, melhorar o retorno sobre o patrimônio, obter ganhos no fluxo de caixa operacional e eliminar desperdícios."
-$(".textoFinanceiro").html(textoFinanceiro);
-
-var textoVendas = "Assegurar o crescimento das vendas, a melhoria da lucratividade e a longevidade dos resultados.  Identificar e padronizar as melhores práticas."
-$(".textoVendas").html(textoVendas);
-
-var textoEstoque = "Equilibrar compras, armazenagem e entregas, controlando as entradas e o consumo de materiais e movimentando o ciclo dos produtos."
-$(".textoEstoque").html(textoEstoque);
-
-
-
-var textoCompras = "Otimizar os custos e o potencial aumento da lucratividade sobre as vendas atraves da padronização do planejamento das compras com base no perfil dos seus clientes."
-$(".textoCompras").html(textoCompras);
-
-var textoProducao = "Levantar e analisar as interrupções nos processos produtivos e as respectivas causas dessas interrupções, viabilizando um maior ganho de produtividade."
-$(".textoProducao").html(textoProducao);
-
-var textoAdministrativo = "Gerenciar as rotinas a fim de permitir que profissionais sejam capazes de resolver problemas do dia a dia e de minimizar a sobrecarga de supervisores e gerentes."
-$(".textoAdministrativo").html(textoAdministrativo);
