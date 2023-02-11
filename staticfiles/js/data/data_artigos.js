992a80f691cde83862bf0c8a3748a3c0fb0d6621
Angelita Santos <angelitasantos@gmail.com>
2023-02-11 19:03:31 -0300

    create dataJson artigos.js

--- staticfiles/js/data/data_artigos.js
+++ staticfiles/js/data/data_artigos.js
@@ -4,7 +4,7 @@ dataJson.map(function (item, index) {
     let dataJsonItem = dqS('.dataJson-models .dataJson-item').cloneNode(true);
 
     dataJsonItem.setAttribute('data-key', index)
-    dataJsonItem.querySelector('.dataJson-item--img img').src = item.img;
+    // dataJsonItem.querySelector('.dataJson-item--img img').src = item.img;
     dataJsonItem.querySelector('.dataJson-item--title').innerHTML = item.title;
     dataJsonItem.querySelector('.dataJson-item--date').innerHTML = item.date;
     dataJsonItem.querySelector('.dataJson-item--resume').innerHTML = item.resume;
