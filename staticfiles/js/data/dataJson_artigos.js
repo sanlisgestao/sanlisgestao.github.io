12e15417ea22ea40cfa948c1bcbe592bff3bfe7b
Angelita Santos <angelitasantos@gmail.com>
2023-02-11 10:35:52 -0300

    create dataJson artigos.js

--- staticfiles/js/data/dataJson_artigos.js
+++ staticfiles/js/data/dataJson_artigos.js
@@ -6,7 +6,7 @@ let dataJson = [
         date: '25/01/2023',
         resume: 'Maior eficiência do processo orçamentário e de controle dos gastos e crescimento da \
                     receita operacional, com melhoria da margem de contribuição.',
-        link: 'templates/planos.html'
+        link: 'templates/artigos.html'
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ let dataJson = [
         date: '26/01/2023',
         resume: 'Padronização dos procedimentos gerenciais e operacionais com aumento da produtividade \
                     e garantia de processos estáveis e padronizados.',
-        link: 'ago/2022 - atual'
+        link: 'templates/artigos.html'
     },
     {
         id: 3,
@@ -24,6 +24,6 @@ let dataJson = [
         date: '27/01/2023',
         resume: 'Foco nos resultados de médio e longo prazo e maximização do lucro e do crescimento \
                     sustentado. Aumento da confiabilidade nos serviços e produtos.',
-        link: 'ago/2022 - atual'
+        link: 'templates/artigos.html'
     }
 ];
