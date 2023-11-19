93ed1cda98519f9f5d2f876b8fb3127f39499ec3
Angelita Santos <angelitasantos@gmail.com>
2023-11-19 18:44:42 -0300

    update template

---
+++ static/js/navegation.js
@@ -0,0 +1,11 @@
+// Header
+
+$('#nav-header').load('../../templates/base/header.html #navegacao section');
+$('#nav-desktop').load('../../templates/base/header.html #navegacao .desktop li');
+$('#nav-mobile').load('../../templates/base/header.html #navegacao .mobile li');
+
+
+
+// Footer
+
+$('#nav-footer').load('../../templates/base/footer.html #rodape');
