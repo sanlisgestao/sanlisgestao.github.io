e3f58e1619f18bb3e59be00b891d84ac14b0aec1
Angelita Santos <angelitasantos@gmail.com>
2023-12-04 09:19:51 -0300

    altera template

--- static/js/main.js
+++ static/js/main.js
@@ -4,24 +4,7 @@
 
 
 var textoPrincipal = "Busca melhorar a eficiência do seu trabalho através do gerenciamento das atividades financeiras, comerciais e administrativas."
-$("#textoPrincipal").html(textoPrincipal);
-
-var btnInfo = "Mais informações"
-$(".btnInfo").html(btnInfo);
-
-var btnSaibaMais = "Saiba Mais"
-$(".btnSaibaMais").html(btnSaibaMais);
-
-var linkInfo = "Continue lendo ..."
-$(".linkInfo").html(linkInfo);
-
-var emConstrucao = "Em Desenvolvimento"
-$(".emConstrucao").html(emConstrucao);
-
-var textoAreaRestrita = "Área Restrita"
-$(".textoAreaRestrita").html(textoAreaRestrita);
-
-
+document.getElementById("textoPrincipal").innerHTML = textoPrincipal;
 
 var textoQuemSomos = "Trabalhamos em parceria presencial com empreendedores, artesãos, produtores, microempreendedores individuais e pequenas empresas com serviços especializados em gerenciamento das atividades financeiras, comerciais, operacionais e administrativas. Nossa equipe atua com os clientes, a fim de identificar os 6 pilares ligados a melhoria dos resultados. Esses pilares são: pessoas, métodos, processos, planejamento, indicadores e tecnologia. Em uma economia competitiva e dinâmica, é cada vez mais importante que a Gestão de um negócio se adapte às novas realidades exigidas pelo mercado e pelos consumidores. Os serviços da SanLis são elaborados de acordo com a realidade de cada negócio identificando e propondo soluções adequadas ao perfil do cliente."
-$(".textoQuemSomos").html(textoQuemSomos);
+document.getElementById("textoQuemSomos").innerHTML = textoQuemSomos;
--- static/js/navegation.js
+++ static/js/navegation.js
@@ -3,5 +3,10 @@ $('#nav-header').load('../../templates/base/header.html #navegacao section');
 $('#nav-desktop').load('../../templates/base/header.html #navegacao .desktop li');
 $('#nav-mobile').load('../../templates/base/header.html #navegacao .mobile li');
 
+
+// Artigos
+$('#load-artigos').load('../../templates/home/artigos.html #artigos');
+
+
 // Footer
 $('#nav-footer').load('../../templates/base/footer.html #rodape');
